98ad48e80c7713c8010d0baac751162a2d916678
kevinEsquivel <42071780+kevinEsquivel@users.noreply.github.com>
2020-10-15 22:29:30 -0600

    Initial commit

---
+++ app/src/main/java/mx/tecnm/tepic/u1_practica1_layout_mas_layouts/MainActivity2.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.tepic.u1_practica1_layout_mas_layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
